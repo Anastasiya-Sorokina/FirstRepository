5b8740c63e0da04aa8fb2efcff52c191a5576956
Anastasiya-Sorokina <114878099+Anastasiya-Sorokina@users.noreply.github.com>
2022-10-03 00:05:21 +0300

    Add files via upload

---
+++ ЛР1 зад.1-а.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	double x, y, z;
+	cout<<"Enter x:";
+	cin>>x;
+	cout<< "Enter y:";
+	cin>>y;
+	z = (1 + abs(x * y)) / (abs(x) + abs(y));
+	cout << "Result " << z << "\n";
+	return(0);
+}
